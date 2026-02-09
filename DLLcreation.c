8de9653c673855d232bebc7d918f56ba2aab77b1
MG Marshall <mg.marshal777@gmail.com>
2026-02-09 23:19:04 +0600

    Enhance doubly linked list with insert and delete functions
    Added functions to insert and delete nodes at various positions in the doubly linked list.

--- DLLcreation.c
+++ DLLcreation.c
@@ -20,6 +20,71 @@ void createDLL(int value)
         tail = newnode;
     }
 }
+void insrertAtBeg(int value)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = value;
+    newnode->prev = NULL;
+    head->prev = newnode;
+    newnode->next = head;
+    head = newnode;
+}
+void insertAnyPos(int pos, int value)
+{
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *temp1, *temp = head;
+    newnode->data = value;
+    int count = 1;
+    while (count < pos - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    temp1 = temp->next;
+    temp1->prev = newnode;
+    temp->next = newnode;
+    newnode->prev = temp;
+    newnode->next = temp1;
+
+}
+void deleteAtBeg()
+{
+    if (head == NULL){
+        printf("List is empty\n");
+        return;
+    }
+    struct Node *newnode = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *temp = head;
+    temp = temp->next;
+    temp->prev = NULL;
+    head = temp;
+}
+void deleteAnyPos(int pos)
+{
+    struct Node *temp = head, *temp1;
+    int count = 1;
+    while (count < pos - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    temp1 = temp->next;
+    temp1 = temp1->next;
+    temp1->prev = temp;
+    temp->next = temp1;
+}
+void deleteAtLast()
+{
+    if (tail == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node *temp = tail;
+    tail = tail->prev;
+    tail->next = NULL;
+    free(temp);
+}
 void display()
 {
     printf("NULL <- ");
@@ -36,9 +101,15 @@ void display()
 }
 int main()
 {
-    createDLL(10);
-    createDLL(30);
-    createDLL(60);
-    display();
+    createDLL(10);       // NULL <- 10 -> NULL
+    createDLL(30);       // NULL <- 10 <-> 30 -> NULL
+    createDLL(60);       // NULL <- 10 <-> 30 <-> 60 -> NULL
+    insrertAtBeg(5);     // NULL <- 5 <-> 10 <-> 30 <-> 60 -> NULL
+    insrertAtBeg(1);     // NULL <- 1 <-> 5 <-> 10 <-> 30 <-> 60 -> NULL
+    insertAnyPos(4, 20); // NULL <- 1 <-> 5 <-> 10 <-> 20 <-> 30 <-> 60 -> NULL
+    deleteAtBeg();       // NULL <- 5 <-> 10 <-> 20 <-> 30 <-> 60 -> NULL
+    deleteAnyPos(4);     // NULL <- 5 <-> 10 <-> 20 <-> 60 -> NULL
+    deleteAtLast();      // NULL <- 5 <-> 10 <-> 20 -> NULL
+    display();           // output == NULL <- 5 <-> 10 <-> 20 -> NULL
     return 0;
 }
